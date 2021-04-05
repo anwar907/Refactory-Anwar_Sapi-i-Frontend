86ea35eaa3a2e06ab5aafc7f3ad6477943379822
anwar907 <rilltheend@gmail.com>
2021-04-05 16:47:34 +0800

    add answer number 9

---
+++ 09.js
@@ -0,0 +1,5 @@
+<script type = "text/javascript">
+          var dataSort = [3, 12, 4, 5, 8, 9];
+          var sorted = dataSort.sort(function(a, b) {return a -b});
+          document.write("Sorting data: " + sorted);
+</script>
